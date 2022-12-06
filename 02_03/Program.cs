1b1c0bf7ec3c442a5f3aba3d00b939cfa67e79d8
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-06 21:48:32 +0300

    02_03

--- 02_03/Program.cs
+++ 02_03/Program.cs
@@ -6,3 +6,21 @@
 
 
 
+Console.Write("Введите номер дня недели  ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    if (num >= 1 && num <= 5)
+    {
+        Console.Write("Это не выходной");
+    }
+
+    else if (num >= 6 && num <= 7)
+    {
+        Console.Write("Это выходной");
+    }
+    else Console.Write("Не существует такого дня недели");
+    break;
+}
