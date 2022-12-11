5eb094c1295c7ecea1c3ce8e9fa956bd87ff8d8e
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-11 14:57:46 +0300

    04_02

--- 04_02/Program.cs
+++ 04_02/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+int a = InputNumber();
+
+Console.Write("Сумма всех цифр в числе равна: " + SumNumber());
+
+//Сумма чисел
+int SumNumber()
+
+{
+    int sum = 0;
+    int i = a;
+    while (i > 0)
+    {
+        int num = i % 10;
+        i = i / 10;
+        sum = sum + num;
+    }
+    return sum;
+}
+
+// Проверяем число или нет
+int InputNumber()
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Введено не число, попробуйте еще раз.");
+    }
+    return number;
+}
