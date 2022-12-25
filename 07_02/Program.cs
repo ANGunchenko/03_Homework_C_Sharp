dc64ece246038cfa09e6a0157c4694e426f0b124
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-25 17:22:46 +0300

    07_03

---
+++ 07_02/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+Console.Clear();
+
+int m = inputNumber("Введите количество строк массива m: ");
+int n = inputNumber("Введите количество столбцов массива n: ");
+
+int line = inputNumber("Введите индекс из строки: ");
+int column = inputNumber("Введите индекс из столбца: ");
+
+const int LeftRange = 0;
+const int RightRange = 10;
+
+int[,] array = FillArray2D(m, n);
+PrintArray2D(array);
+Console.WriteLine();
+SearchingForAnItemByIndexes();
+
+
+void SearchingForAnItemByIndexes() // Ищем и выводим в консоль элемент по индексам массива
+{
+    if (line < array.GetLength(0) && column < array.GetLength(1))
+
+        Console.WriteLine($"| {array[line, column]} | - это элемент массива с индексами | {line} | {column} |");
+
+    else Console.WriteLine($"| {line} | {column} | - элемента с такими индексами нет в массиве");
+}
+
+int inputNumber(string str) // Проверяем чтобы вводилось число
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+int[,] FillArray2D(int m, int n) //  Заполняем двумерный массив
+{
+    Random rand = new Random();
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(LeftRange, RightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray2D(int[,] array) // Выводим двумерный массив в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   |   ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 07_03/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+Console.Clear();
+
+int m = inputNumber("Введите количество строк массива m: ");
+int n = inputNumber("Введите количество столбцов массива n: ");
+
+const int LeftRange = 0;
+const int RightRange = 10;
+
+int[,] array = FillArray2D(m, n);
+PrintArray2D();
+ArithmeticMeanColumnElements();
+
+void ArithmeticMeanColumnElements() // Вычисляем среднее арифметическое в каждом столбце и выводим в консоль
+{for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine($"Среднее арифметическое {j + 1} столбца = {Math.Round((sum / array.GetLength(0)), 2)}");
+}
+}
+int inputNumber(string str) // Проверяем чтобы вводилось число
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+int[,] FillArray2D(int m, int n) //  Заполняем двумерный массив
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(LeftRange, RightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray2D() // Выводим двумерный массив в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   |   ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
